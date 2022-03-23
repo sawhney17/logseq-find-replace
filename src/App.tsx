08f71b9b145f78ef3a1f5e205ba5279e383692e3
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-03-23 15:46:18 +0400

    feat(options): support find and replace IN page feat(options): support toggling case sensitivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,83 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useAppVisible } from "./utils";
-// import {
-//   IBatchBlock,
-// } from "@logseq/libs/dist/LSPlugin.user";
 function App() {
-  // const innerRef = useRef<HTMLDivElement>(null);
   const visible = useAppVisible();
-  if (visible) {
-    // const [formValues, setFormValues] = useState("");
-    var isOn = false;
-    var searchAvailable = false;
-    var formValues = "";
-    var replaceValues = "";
-    var freezeEntry = true;
-    var results = [];
-    function changeValue(e: any) {
-      formValues = e.target.value;
-      console.log("hi");
-      console.log(formValues);
+  const [scopePage, setScopePage] = useState(true);
+  const [formValues, setFormValues] = useState("");
+  const [replaceValues, setReplaceValues] = useState("");
+  const [caseInSensitivity, setCaseInSensitivity] = useState(true);
+  const [freezeEntry, setFreezeEntry] = useState(true);
+  const [searchAvailable, setSearchAvailable] = useState(false);
+  const [results, setQueryResults] = useState([]);
 
+  function checkEnableability() {
+    var replaceEntry = document.getElementById("Replace Status")!;
+    if (searchAvailable == true && replaceValues != "" && results.length != 0) {
+      replaceEntry.className = replaceEntry.className.replace(
+        "opacity-20",
+        "opacity-100"
+      );
+      setFreezeEntry(false);
+    } else {
+      replaceEntry.className = replaceEntry.className.replace(
+        "opacity-100",
+        "opacity-20"
+      );
+      setFreezeEntry(true);
+    }
+  }
+  function databaseScope() {
+    if (!scopePage) {
+      return "current page";
+    } else {
+      return "entire database";
+    }
+  }
+
+  React.useEffect(() => {
+    if (visible) {
+      checkEnableability();
+      document.getElementById("titleLabel")!.innerHTML =
+        "Searching in " + databaseScope();
+    }
+  });
+  if (visible) {
+    // var setSearchAvailable(false);
+    // var setFreezeEntry(true);
+
+    function changeValue(e: any) {
+      setFormValues(e.target.value);
+      document.getElementById("matchCount")!.innerHTML = `Matches: ?`;
       if (formValues != "") {
-        searchAvailable = false;
+        setSearchAvailable(false);
       }
       checkEnableability();
     }
     function changeReplaceValue(e: any) {
-      replaceValues = e.target.value;
+      setReplaceValues(e.target.value);
+      //wait 100 ms and then check enableability
       checkEnableability();
     }
-    function checkEnableability() {
-      var replaceEntry = document.getElementById("Replace Status")!;
-      if (
-        searchAvailable == true &&
-        replaceValues != "" &&
-        results.length != 0
-      ) {
-        replaceEntry.className = replaceEntry.className.replace(
-          "opacity-20",
-          "opacity-100"
-        );
-        freezeEntry = false;
-      } else {
-        replaceEntry.className = replaceEntry.className.replace(
-          "opacity-100",
-          "opacity-20"
-        );
-        freezeEntry = true;
-      }
+    function scopeToggle() {
+      setSearchAvailable(false);
+      checkEnableability();
+      document.getElementById("matchCount")!.innerHTML = `Matches: ?`;
+      document.getElementById("titleLabel")!.innerHTML =
+        "Searching in " + databaseScope();
+      setScopePage(!scopePage);
     }
-    function searchClicked() {
-      logseq.DB.datascriptQuery(
-        `
+    function caseToggle() {
+      setSearchAvailable(false);
+      document.getElementById("matchCount")!.innerHTML = `Matches: ?`;
+
+      checkEnableability();
+      setCaseInSensitivity(!caseInSensitivity);
+    }
+    async function searchClicked() {
+      if (scopePage) {
+        if (caseInSensitivity) {
+          logseq.DB.datascriptQuery(
+            `
       [:find (pull ?b [*])
          :where
          [?b :block/content ?c]
@@ -58,52 +85,123 @@ function App() {
          [(re-find ?p ?c)]
        ]
       `
-      ).then((result) => {
+          ).then((result) => {
+            performMatches(result);
+          });
+        } else {
+          logseq.DB.datascriptQuery(
+            `
+            [:find (pull ?b [*])
+            :where
+            [?b :block/content ?c]
+            [(re-pattern "hello") ?p]
+            [(re-find ?p ?c)]
+          ]
+      `
+          ).then((result) => {
+            performMatches(result);
+          });
+        }
+      } else {
+        let block = (await logseq.Editor.getCurrentPage()).name;
+        console.log(block);
+        let pageName = block;
+        console.log(`[:find (pull ?b [*])
+        :where
+        [?b :block/page [:block/name "${pageName}"]]
+        [?b :block/content ?c]
+        [(re-pattern "(?i)${formValues}") ?p]
+  [(re-find ?p ?c)]
+]`);
+        if (caseInSensitivity) {
+          logseq.DB.datascriptQuery(
+            `[:find (pull ?b [*])
+               :where
+               [?b :block/page [:block/name "${pageName}"]]
+               [?b :block/content ?c]
+               [(re-pattern "(?i)${formValues}") ?p]
+         [(re-find ?p ?c)]
+ ]`
+          ).then((result) => {
+            performMatches(result);
+          });
+        } else {
+          logseq.DB.datascriptQuery(
+            `
+            [:find (pull ?b [*])
+               :where
+               [?b :block/page [:block/name "${pageName}"]]
+               [?b :block/content ?c]
+               [(re-pattern "${formValues}") ?p]
+         [(re-find ?p ?c)]
+ ]
+      `
+          ).then((result) => {
+            performMatches(result);
+          });
+        }
+      }
+      function performMatches(result) {
         document.getElementById(
           "matchCount"
         )!.innerHTML = `Matches: ${result.length}`;
-        searchAvailable = true;
+        setSearchAvailable(true);
         checkEnableability();
-        results = result;
-      });
+        setQueryResults(result);
+      }
     }
     function replaceClicked() {
-      var retVal = confirm(
-        `Are you sure you want to replace all instances of "${formValues}" with "${replaceValues}" across ${results.length} blocks?`
-      );
-      if (retVal == true) {
-        for (const x in results) {
-          console.log(results[x]);
-          const uuid = results[x][0].uuid["$uuid$"];
-          console.log(uuid);
-          const content = results[x][0].content;
-          console.log(content);
-          logseq.Editor.updateBlock(
-            results[x][0].uuid["$uuid$"],
-            results[x][0].content.replaceAll(formValues, replaceValues)
-          );
-          logseq.hideMainUI();
-        }
-
-        logseq.App.showMsg(
-          `Successfully replaced in ${results.length} blocks", "success`
+      if (freezeEntry == false) {
+        var retVal = confirm(
+          `Are you sure you want to replace all instances of "${formValues}" with "${replaceValues}" across ${results.length} blocks?`
         );
-      } else {
-        logseq.hideMainUI();
-        logseq.App.showMsg("Replacement Operation Cancelled", "error");
+        if (retVal == true) {
+          for (const x in results) {
+            console.log(results[x]);
+            const uuid = results[x][0].uuid["$uuid$"];
+            console.log(uuid);
+            const content = results[x][0].content;
+            console.log(content);
+            var regex;
+            if (caseInSensitivity) {
+              regex = new RegExp(formValues, "ig");
+            } else {
+              regex = new RegExp(formValues, "g");
+            }
+            console.log(regex);
+            console.log(content.replaceAll(regex, replaceValues));
+            logseq.Editor.updateBlock(
+              results[x][0].uuid["$uuid$"],
+              results[x][0].content.replaceAll(regex, replaceValues)
+            );
+            logseq.hideMainUI();
+          }
+
+          logseq.App.showMsg(
+            `Successfully replaced in ${results.length} blocks", "success`
+          );
+        } else {
+          logseq.hideMainUI();
+          logseq.App.showMsg("Replacement Operation Cancelled", "error");
+        }
       }
     }
     return (
       <div className="grid place-items-center justify-center h-screen">
         <div className="bg-gray-400 items-center rounded-md p-4 max-w-6/10">
+          <label className="text-3xl font-bold" id="titleLabel">
+            Searching in current page
+          </label>
+          <br></br>
+          <br></br>
           <div className="grid grid-cols-2 gap-4 place-items-auto">
             {/* <div className="grid place-items-center"> */}
             <div>
-              <h1 className="font-bold text-size-20px p-3">Find</h1>
+              <h1 className="font-bold text-size-23px p-3">Find</h1>
             </div>
             <input onChange={changeValue} className="rounded-md"></input>
             <label className="px-4" id="matchCount">
-              Matches: 0
+              Matches: ?
             </label>
             <button
               onClick={searchClicked}
@@ -113,7 +211,7 @@ function App() {
             </button>
             {/* </div> */}
             {/* <div className="grid place-items-center"> */}
-            <h1 className="font-bold text-size-20px p-3">Replace</h1>
+            <h1 className="font-bold text-size-23px p-3">Replace</h1>
             <input onChange={changeReplaceValue} className="rounded-md"></input>
 
             <label className="px-4">
@@ -127,6 +225,22 @@ function App() {
             >
               Replace All!
             </button>
+            <div>
+              <input
+                type="checkbox"
+                checked={scopePage}
+                onChange={scopeToggle}
+              ></input>{" "}
+              Search ENTIRE database
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                checked={caseInSensitivity}
+                onChange={caseToggle}
+              ></input>{" "}
+              Ignore Case(Case insensitive)?
+            </div>
           </div>
         </div>
         <br></br>
